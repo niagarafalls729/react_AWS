60a23c717ae191327aa443cf4dacd678a0f342f0
jisukim92 <jisu.kim92@hancommds.com>
2022-09-16 17:31:17 +0900

    회사 소스 커밋

--- my-mini-blog/src/App.js
+++ my-mini-blog/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Sidebar from './sidebar/Sidebar'; 
 function App() {
   return (
     <div className="App">
       <p>dpfjttttttdpfjdpfjdpfj</p>
-      <p>왜 aws 빌드를 못하는ㄱ ㅓ뉘!</p>
+      <p>왜 aws 빌드를 못하는ㄱ ㅓ뉘!</p> 
+      <Sidebar></Sidebar>
+      
+
     </div>
   );
 }
