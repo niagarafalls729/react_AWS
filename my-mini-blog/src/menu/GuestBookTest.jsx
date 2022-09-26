27589b88e5022af70d2f69f33eb68ac4bc3d764d
niagarafalls729 <wltn729@gmail.com>
2022-09-26 17:00:09 +0900

    crud li 태그로 만들었다가 수정수정 table 로 진행중.

---
+++ my-mini-blog/src/menu/GuestBookTest.jsx
@@ -0,0 +1,106 @@
+import { useRef, useState, useEffect, useId } from "react";
+// 파이어베이서 파일에서 import 해온 db
+import { db } from "../FireBase/DBconfig";
+// db에 접근해서 데이터를 꺼내게 도와줄 친구들
+import { collection, getDocs, addDoc,updateDoc,doc, deleteDoc } from "firebase/firestore/lite";
+
+
+function Guestbook() {
+  //R 로 쓰는 값
+  const [users, setUsers] = useState([]);
+  // db의 users 컬렉션을 가져옴
+  const usersCollectionRef = collection(db, "users");
+  // 유니크 id를 만들기 위한 useId(); - react 18 기능으로, 이 훅을 이렇게 사용하는게 맞고 틀린지는 모른다.
+  const uniqueId = useId();
+  // C U D 행동하면 한번씩 렌더링을 위한 값 useEffect에 사용.
+  const [changed, setChanged] = useState(false);
+  // 시작될때 한번만 실행
+  useEffect(() => {
+    // 비동기로 데이터 받을준비
+    const getUsers = async () => {
+      // getDocs로 컬렉션안에 데이터 가져오기
+      const data = await getDocs(usersCollectionRef);
+      //console.log("data:::"+JSON.stringify(data))
+      // users에 data안의 자료 추가. 객체에 id 덮어씌우는거
+      setUsers(data.docs.map((findDoc) => ({ ...findDoc.data(), id: findDoc.id })));
+    };
+    setChanged(false);
+    getUsers(); 
+
+  }, [changed]);
+
+  const [userId, SetUserId] = useState("");
+  const [age, SetAge] = useState("");
+
+  // 띄워줄 데이터 key값에 고유ID를 넣어준다.
+  const showUsers = users.map(
+    (value) =>
+      value && (
+        <div>
+          <div key={uniqueId}>
+            <h1>Name: {value.name}</h1>
+            <h1>Age: {value.age}</h1>
+            <h1>Age: {value.hi}</h1>
+            <button
+              onClick={() => {
+                updateUser(value.id, value.age);
+              }}
+            >
+              Increase Age
+            </button>
+            <button onClick={() => {deleteUser(value.id)}}>
+              Delete Users
+            </button>
+          </div>
+        </div>
+      )
+  );
+
+  const saveInput = useRef();
+  const saveInputAge = useRef();
+
+  async function fn_save() {
+    // 여기다가 하니깐 useState 가 재 렌더링 하면서 값응ㅇㄹ 못넣음 시발
+
+    // SetUserId(saveInput.current.value);
+    // SetAge(saveInputAge.current.value);
+    // addDoc을 이용해서 내가 원하는 collection에 내가 원하는 key로 값을 추가한다.
+    //console.log("userId" + userId + "/" + "age" + age);
+    await addDoc(usersCollectionRef, { name: userId, age: age });
+    setChanged(true);
+  }
+ async function updateUser(id, age){
+  //id 로 해당 디비중에 값을 찾음.
+  const userDoc = doc(db, "users", id)
+  const newField = {age: ( Number(age) + 1)};
+  await updateDoc(userDoc, newField);
+  setChanged(true);
+ }
+async function deleteUser(id){
+  const userDoc = doc(db, "users", id)
+  await deleteDoc(userDoc)
+  setChanged(true);
+}
+  return (
+    <>
+      <input
+        type="text"
+        ref={saveInput}
+        onChange={(e) => {
+          SetUserId(e.target.value);
+        }}
+      ></input>
+      <input
+        type="number"
+        ref={saveInputAge}
+        onChange={(e) => {
+          SetAge(e.target.value);
+        }}
+      ></input>
+      <button onClick={fn_save}>저장</button>
+      <div className="App">{showUsers}</div>
+    </>
+  );
+}
+
+export default Guestbook;
--- my-mini-blog/src/menu/Guestbook.jsx
+++ my-mini-blog/src/menu/Guestbook.jsx
@@ -1,103 +1,77 @@
-import { useRef, useState, useEffect, useId } from "react";
-// 파이어베이서 파일에서 import 해온 db
-import { db } from "../FireBase/DBconfig";
-// db에 접근해서 데이터를 꺼내게 도와줄 친구들
-import { collection, getDocs, addDoc,updateDoc,doc, deleteDoc } from "firebase/firestore/lite";
-import { async } from "@firebase/util";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useRef, useState } from "react";
+import Table from 'react-bootstrap/Table';
 
-function Guestbook() {
-  // 이따가 users 추가하고 삭제하는거 진행을 도와줄 state
-  const [users, setUsers] = useState([]);
-  // db의 users 컬렉션을 가져옴
-  const usersCollectionRef = collection(db, "users");
-  // 유니크 id를 만들기 위한 useId(); - react 18 기능으로, 이 훅을 이렇게 사용하는게 맞고 틀린지는 모른다.
-  const uniqueId = useId();
-  console.log(uniqueId);
-  const [changed, setChanged] = useState(false);
-  // 시작될때 한번만 실행
-  useEffect(() => {
-    // 비동기로 데이터 받을준비
-    const getUsers = async () => {
-      // getDocs로 컬렉션안에 데이터 가져오기
-      const data = await getDocs(usersCollectionRef);
-      // users에 data안의 자료 추가. 객체에 id 덮어씌우는거
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    setChanged(false);
-    getUsers();
-  }, [changed]);
+function ActiveExample() {
+  const mo = useRef([]);
+  const [choiceState, SetChoiceState] = useState(false);
 
-  const [userId, SetUserId] = useState("");
-  const [age, SetAge] = useState("");
-
-  // 띄워줄 데이터 key값에 고유ID를 넣어준다.
-  const showUsers = users.map(
-    (value) =>
-      value && (
-        <div>
-          <div key={uniqueId}>
-            <h1>Name: {value.name}</h1>
-            <h1>Age: {value.age}</h1>
-            <button
-              onClick={() => {
-                updateUser(value.id, value.age);
-              }}
-            >
-              Increase Age
-            </button>
-            <button onClick={() => {deleteUser(value.id)}}>
-              Delete Users
-            </button>
-          </div>
-        </div>
-      )
-  );
-
-  const saveInput = useRef();
-  const saveInputAge = useRef();
-
-  async function fn_save() {
-    // 여기다가 하니깐 useState 가 재 렌더링 하면서 값응ㅇㄹ 못넣음 시발
-
-    // SetUserId(saveInput.current.value);
-    // SetAge(saveInputAge.current.value);
-    // addDoc을 이용해서 내가 원하는 collection에 내가 원하는 key로 값을 추가한다.
-    console.log("userId" + userId + "/" + "age" + age);
-    await addDoc(usersCollectionRef, { name: userId, age: age });
-    setChanged(true);
+  function fn_mouseOver(key) {
+    //이건 마우스 위로 올렸을때 효과주기 연습.
+    key.target.style.background = "blue";
+    key.target.style.color = "white";
   }
- async function updateUser(id, age){
-  //id 로 해당 디비중에 값을 찾음.
-  const userDoc = doc(db, "users", id)
-  const newField = {age: ( Number(age) + 1)};
-  await updateDoc(userDoc, newField);
-  setChanged(true);
- }
-async function deleteUser(id){
-  const userDoc = doc(db, "users", id)
-  await deleteDoc(userDoc)
-  setChanged(true);
-}
+
+  function fn_mouseLeave(key) {
+    key.target.style.background = "white";
+    key.target.style.color = "black";
+  }
+  function fn_click() {
+    // 게시글 선택
+    SetChoiceState(true);
+    console.log("fn_click");
+  }
+  function contentList() {
+    return (
+      <Table striped bordered hover variant="dark" >
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>제목</th>
+            <th></th>
+            <th>시간</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td></td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td></td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td></td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  }
+
+  function choiceContent() {
+    return (
+      <div>
+        <h1>게시글</h1>
+      </div>
+    );
+  }
   return (
     <>
-      <input
-        type="text"
-        ref={saveInput}
-        onChange={(e) => {
-          SetUserId(e.target.value);
-        }}
-      ></input>
-      <input
-        type="number"
-        ref={saveInputAge}
-        onChange={(e) => {
-          SetAge(e.target.value);
-        }}
-      ></input>
-      <button onClick={fn_save}>저장</button>
-      <div className="App">{showUsers}</div>
+      {choiceState && <div>{choiceContent()}</div>}
+      {contentList()}
     </>
   );
 }
 
-export default Guestbook;
+export default ActiveExample;
