245ebfe4408a07de9ed44a0ee560f5a125b973d7
niagarafalls729 <wltn729@gmail.com>
2022-10-04 23:00:41 +0900

    댓글 불러오기 , 생성까지 완료했으나, where orderby 쓰면 에러가뜸 ...?

--- my-mini-blog/src/component/CommentCRUD.jsx
+++ my-mini-blog/src/component/CommentCRUD.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import { useRef, useState ,useId} from "react";
+import { useRef, useState, useId, useEffect } from "react";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 import {
@@ -17,55 +17,110 @@ import {
   fromDate,
   where,
 } from "firebase/firestore/lite";
+import { db } from "../FireBase/DBconfig";
 function Comment(props) {
   // 이 페이지 기준에서는 전역변수로 받아오기에 이렇게만해도 작동됨
 
-  const comment_val = useRef();
+  const content = useRef();
+  const name = useRef();
+  const pw = useRef();
+  const capcha = useRef();
 
-  const capcha =useRef();
-  
   const uniqueId = useId();
 
-  const  random= Math.floor(Math.random() * 3*1000)
+  const random = Math.floor(Math.random() * 3 * 1000);
   const fn_randomNumCreaated = (
-      <Form.Label column sm={1} style={{ paddingLeft: "10px" }} key={uniqueId}>
-          {random}
-      </Form.Label>
-  )
-
-  function fn_created_comment() {
-  
-    console.log("em"+random+"?///"+capcha.current.value)
+    <Form.Label column sm={1} style={{ paddingLeft: "10px" }} key={uniqueId}>
+      {random}
+    </Form.Label>
+  );
+  const [users, setUsers] = useState([]);
+  const [change, setChange] = useState(false);
 
 
+  useEffect(() => {
+    // 비동기로 데이터 받을준비
+    const getUsers = async () => {
+      const q = query(
+        collection(db, "comment"),
+        where("guestbook_key", "==", props.value),
+        // orderBy("comment_key","asc") ,
+        
+      );
+      // getDocs로 컬렉션안에 데이터 가져오기
+      const data = await getDocs(q);
+      //console.log("data:::"+JSON.stringify(data))
+      // users에 data안의 자료 추가. 객체에 id 덮어씌우는거
+      setUsers(
+        data.docs.map((findDoc) => ({ ...findDoc.data(), id: findDoc.id }))
+      );
+
+      // users.map((value) => console.log(value.content));
+    };
+    getUsers();
+  }, [change]);
+
+  const [user, setUser] = useState();
+
+  async function fn_created_comment() {
+    console.log("em" + random + "?///" + capcha.current.value);
+    // if (random != capcha.current.value) {
+    //   return alert("자동입력방지 번호가 틀렸습니다.!");
+    // }
+
+    const usersCollectionRef = collection(db, "comment");
+    const q = query(
+      collection(db, "comment"),
+      orderBy("comment_key", "desc"),
+      limit(1)
+    );
+    const data2 = await getDocs(q);
+
+    const maxnum = data2.docs.map((findDoc) => ({
+      ...findDoc.data(),
+      id: findDoc.id,
+    }));
+
+    // console.log("save시도"+maxnum[0].comment_key);
+
+    await addDoc(usersCollectionRef, {
+      comment_key: maxnum[0].comment_key + 1,
+      content: content.current.value,
+      name: name.current.value,
+      created: new Date(),
+      guestbook_key: props.value,
+      password: pw.current.value,
+    });
+    setChange(true);
   }
 
   return (
     <>
+      {users.map((value) => value && <h5>댓글 : {value.content}</h5>)}
+
       <FloatingLabel controlId="floatingTextarea2" label="Comments">
         <Form.Control
+          ref={content}
           as="textarea"
           placeholder="Leave a comment here"
           style={{ height: "100px" }}
         />
 
-        <div class="input-group input-group-sm">
+        <div className="input-group input-group-sm">
+          <Col xs="2">{fn_randomNumCreaated}</Col>
           <Col xs="2">
-           {fn_randomNumCreaated}
-          </Col>
-          <Col xs="3">
             <input
-              maxlength="10"
+              maxLength="10"
               placeholder="왼쪽번호입력!"
               class="form-control"
               ref={capcha}
             />
           </Col>
-          <Col xs="3">
-            <input maxlength="10" placeholder="작성자" class="form-control" />
+          <Col xs="4">
+            <input ref={name} maxLength="10" placeholder="작성자" class="form-control" />
           </Col>
           <Col xs="2">
-            <input maxlength="10" placeholder="비밀번호" class="form-control" />
+            <input ref={pw} maxLength="10" placeholder="비밀번호" class="form-control" />
           </Col>
           <Col>
             <button
